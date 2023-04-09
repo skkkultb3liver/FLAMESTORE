56c18a797cd295fd593ba6e48350999c2100150a
skkkultb3liver <vampirexxxwag@gmail.com>
2023-04-09 15:39:13 +0500

    User activation. Активация пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.mail import EmailMessage
+
 from .forms import RegisterForm
 from django.contrib import messages, auth
 
@@ -19,14 +27,33 @@ def register(request):
             password = form.cleaned_data['password']
             username = email.split('@')[0]
 
-
-            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password, )
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password,
+            )
             user.phone_number = phone_number
 
             user.save()
-            messages.success(request, 'Registration successful!')
 
-            return redirect('register')
+            # ACTIVATION
+            current_site = get_current_site(request)
+            mail_subj = "Account activation"
+            msg = render_to_string('accounts/email_verify.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subj, msg, to=[to_email])
+            send_email.send()
+
+            # messages.success(request, f'An email was sent to {to_email} to activate your account.')
+
+            return redirect(f'/accounts/register/?command=verification&email={email}')
 
     else:
         form = RegisterForm()
@@ -63,3 +90,22 @@ def logout(request):
     messages.success(request, "You're logged out.")
 
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your account is activated!')
+
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+
+        return redirect('register')
--- flameprojects/settings.py
+++ flameprojects/settings.py
@@ -152,4 +152,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'wrong',
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'flamestore900@gmail.com'
+EMAIL_HOST_PASSWORD = 'ebkuqybbxhisvxbz'
+
 
